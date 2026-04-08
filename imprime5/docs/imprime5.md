7405f7d48224b1ee67dba182383f5869412be586
Pedro-enjinia <tomazini.engpedro@gmail.com>
2026-04-07 21:40:06 -0300

    Add files via upload

---
+++ imprime5/docs/imprime5.md
@@ -0,0 +1,16 @@
+# Documentação
+
+Programa imprime5
+Descrição: Este programa os números inteiros de 1 até 5.
+Autor: Pedro J. Tomazini
+Data: 07/04/2026
+Versão: 0.0.1
+
+## Introdução
+
+
+## Funcionalidade
+
+
+## Conclusão
+
---
+++ imprime5/imprime5/imprime_001.py
@@ -0,0 +1,21 @@
+"""
+Programa imprime5
+Descrição: Este programa os números inteiros de 1 até 5.
+Autor: Pedro J. Tomazini
+Data: 07/04/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+i = 0
+
+# Entrada de dados
+
+# Processamento de dados
+
+# Saída de dados
+
+while i<5:
+    print(i+1)
+    i = i+1
